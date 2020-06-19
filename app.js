ed8a98a0adfd08a8e3054302da1cc4951ea96769
Agustin Fiori <afiori@clarika.com.ar>
2020-06-19 19:55:11 -0300

    A new start... again

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+    PORT,
+    console.log(`Server in ${ process.env.NODE_ENV } mode on port ${ PORT }`)
+);
